6cbaccd0619ede679f627efd325d757af5708d03
root <root@DESKTOP-FUPLPL4.localdomain>
2019-10-02 23:29:30 +0300

    Added Automation on requirements

--- requirements/requirements.sh
+++ requirements/requirements.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 sudo apt-get install python3 -y
 printf "\n\n"
-sudo apt-get install pythin3-pip
+sudo apt-get install python3-pip
 printf "\n\n"
 sudo apt-get install swig -y
+printf "\n\n"
+sudo apt-get install libmagickwand-dev -y
 printf "\n\n"
 sudo apt-get install python3-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr \
 flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libpulse-dev -y
 printf "\n\n"
 pip3 install -r requirements.txt
+replace '<policy domain="coder" rights="none" pattern="PDF" />' '<policy domain="coder" rights="read" pattern="PDF" />' -- /etc/ImageMagick-6/policy.xml
+cd ..
+echo "Enter pdf file path"
+read file
+./extractscript.py $file
